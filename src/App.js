cda8792b04c1e7c5eb08bb62713b29638b894244
bruh-123 <brunomriquelme@gmail>
2021-08-17 21:49:43 -0300

    Ready to start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className="App"></div>;
 }
 
 export default App;
+// Order Summary
+
+//   You can now listen to millions of songs, audiobooks, and podcasts on any
+//   device anywhere you like!
+
+//   Annual Plan
+//   $59.99/year
+
+//   Change
+
+//   Proceed to Payment
+//   Cancel Order
+
+// Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+//     Coded by <a href="#">Bruno Riquelme</a>.
